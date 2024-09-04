564cb5ae4e25759fffaf7c0125021807c691a334
Mehdi <mehdifaiz42@outlook.com>
2024-09-04 23:15:40 +0200

    basic port scanner copied from internet

--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"strconv"
+	"time"
+	"os"
 )
 
-func main(){
-	fmt.Printf("PORT SCANNER\n")
+func scanner(server string) []int{
+	var avalible_ports []int
+	for i := 1; i <= 65535; i++{
+		ip := server + ":" + strconv.Itoa(i)
+		_, err := net.DialTimeout("tcp", ip, time.Duration(300)*time.Millisecond)
+		if err == nil{
+			avalible_ports = append(avalible_ports, i)
+		}
+	}
+	return avalible_ports
+}
 
+func main() {
+	fmt.Println("Cheking for available ports...")
+    ports := scanner(os.Args[1])
+    fmt.Println("Ports available: " ,ports)
 }
